723dd30998a3a6bbe00bd3e3aa12339946cce04e
Tristan Malmenayde <tristanmalmenayde@hotmail.fr>
2018-03-14 18:27:30 +0100

    SIGNUP SUITE

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
-import { View, StyleSheet } from 'react-native';
-import SignUpForm from '../components/SignUpForm';
-import SignInForm from '../components/SignInForm';
+import { ScrollView, StyleSheet } from 'react-native';
 
-export default class ProfileScreen extends React.Component {
+
+export default class CalendrierScreen extends React.Component {
   static navigationOptions = {
     title: 'Profile',
   };
 
   render() {
-
     return (
-      <View>
-
-        <SignUpForm onSubmit={this.signup}/>
-        <SignInForm onSubmit={this.signin}/>
-
-      </View>
+      <ScrollView>
+      </ScrollView>
     );
-
   }
 }
