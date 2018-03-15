74949cf9d7a7f3fa0ffee9a9b133c423b50af5a2
Tristan Malmenayde <tristanmalmenayde@hotmail.fr>
2018-03-15 09:30:23 +0100

    Star

--- screens/EvenementScreen.js
+++ screens/EvenementScreen.js
@@ -1,48 +1,19 @@
 import React, { Component } from 'react';
-import {Image, Platform, ScrollView, StyleSheet, Text, TouchableOpacity, View, Avatar } from 'react-native';
+import {Image, Platform, ScrollView, StyleSheet, TouchableOpacity, View, Avatar } from 'react-native';
 import { Container, Header, Content, List, ListItem, Left, Body, Right, Thumbnail, Text } from 'native-base';
 import { List, ListItem } from 'react-native-elements';
 import { WebBrowser } from 'expo';
 import {connect} from 'react-redux';
 import WelcomeScreen from './WelcomeScreen'
 import { MonoText } from '../components/StyledText';
-// import { GiftedChat } from 'react-native-gifted-chat'
-//
-// export default class EvenementScreen extends React.Component {
-//   static navigationOptions = {
-//     header: null,
-//   };
-//
-//   state = {
-//       messages: [],
-//     }
-//
-//     componentWillMount() {
-//       this.setState({
-//         messages: [
-//           {
-//             _id: 1,
-//             text: 'Hello developer',
-//             createdAt: new Date(),
-//             user: {
-//               _id: 2,
-//               name: 'React Native',
-//               avatar: 'https://facebook.github.io/react/img/logo_og.png',
-//             },
-//           },
-//         ],
-//       })
-//     }
-//
-//     onSend(messages = []) {
-//       this.setState(previousState => ({
-//         messages: GiftedChat.append(previousState.messages, messages),
-//       }))
-//     }
 
+export default class EvenementScreen extends React.Component {
+  static navigationOptions = {
+    header: null,
+  };
 
   render () {
-  return (
+    return (
     <Container>
        <Header />
        <Content>
@@ -62,13 +33,6 @@ import { MonoText } from '../components/StyledText';
                  borderWidth: 0,
                  borderRadius: 5,}}
                />
-               {/* <GiftedChat
-                 messages={this.state.messages}
-                 onSend={messages => this.onSend(messages)}
-                 user={{
-                   _id: 1,
-                 }}
-               /> */}
              </Body>
            </ListItem>
          </List>
@@ -76,7 +40,7 @@ import { MonoText } from '../components/StyledText';
     </Container>
    )
   }
-
+}
 
 const styles = StyleSheet.create({
   container: {
